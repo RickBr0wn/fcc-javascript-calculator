4c6922500eff34c3e27d24470c56a8517c0472d6
RiCK BROWN <ricky.brown.00@gmail.com>
2018-04-02 22:35:46 +0100

    Updated HTML with button functions

--- FCC Javascript Calculator/calc.js
+++ FCC Javascript Calculator/calc.js
@@ -1,2 +1,19 @@
 'use strict';
+let entryString = "";
+let sum = "";
+let firstNumber = 0;
+let secondNumber = 0;
 
+function numberEntry(num) {
+    entryString += num;
+    document.getElementById('displayWindow').innerHTML = entryString; 
+}
+
+function clearNumber() {
+    entryString = "";
+    document.getElementById('displayWindow').innerHTML = "0";
+}
+
+function addNumbers() {
+    sum += entryString;
+}
