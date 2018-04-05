ec85ce99c85a7c8986a4fa368169fe28c346f504
RiCK BROWN <ricky.brown.00@gmail.com>
2018-04-05 22:11:02 +0100

    Working, but not adhering to USER STORIES

--- FCC Javascript Calculator/calc.js
+++ FCC Javascript Calculator/calc.js
@@ -1,19 +1,68 @@
 'use strict';
-let entryString = "";
-let sum = "";
-let firstNumber = 0;
-let secondNumber = 0;
+let numberString = "";
+let sums = [];
+let operations = [];
+let answer = 0;
 
 function numberEntry(num) {
-    entryString += num;
-    document.getElementById('displayWindow').innerHTML = entryString; 
+    numberString += num;
+    document.getElementById('displayWindow').innerHTML = numberString;
 }
 
 function clearNumber() {
-    entryString = "";
+    numberString = "";
+    document.getElementById('displayWindow').innerHTML = "0";
+}
+
+function clearMemory() {
+    numberString = "";
+    sums = [];
+    operations = [];
     document.getElementById('displayWindow').innerHTML = "0";
 }
 
 function addNumbers() {
-    sum += entryString;
+    sums.push(parseInt(numberString));
+    operations.push("+");
+    clearNumber();
+}
+
+function minusNumbers() {
+    sums.push(parseInt(numberString));
+    operations.push("-");
+    clearNumber();
+}
+
+function divideNumbers() {
+    sums.push(parseInt(numberString));
+    operations.push("/");
+    clearNumber();
+}
+
+function multiplyNumbers() {
+    sums.push(parseInt(numberString));
+    operations.push("*");
+    clearNumber();
+}
+
+function equalNumbers() {
+    sums.push(parseInt(numberString));
+    switch (operations[0]) {
+        case "+":
+            answer = sums[0] + sums[1];
+            document.getElementById('displayWindow').innerHTML = answer;
+            break;
+        case "-":
+            answer = sums[0] - sums[1];
+            document.getElementById('displayWindow').innerHTML = answer;
+            break;
+        case "/":
+            answer = sums[0] / sums[1];
+            document.getElementById('displayWindow').innerHTML = answer;
+            break;
+        case "*":
+            answer = sums[0] * sums[1];
+            document.getElementById('displayWindow').innerHTML = answer;
+            break;
+    }
 }
