bc14912dc5022ddb4e8e5cae15bb7f94f21ca447
RiCK BROWN <ricky.brown.00@gmail.com>
2018-04-07 15:17:03 +0100

    working version

--- FCC Javascript Calculator/calc.js
+++ FCC Javascript Calculator/calc.js
@@ -1,7 +1,6 @@
 'use strict';
 let numberString = "";
-let sums = [];
-let operations = [];
+let sum = [];
 let answer = 0;
 
 function numberEntry(num) {
@@ -16,53 +15,39 @@ function clearNumber() {
 
 function clearMemory() {
     numberString = "";
-    sums = [];
-    operations = [];
+    sum = [];
     document.getElementById('displayWindow').innerHTML = "0";
 }
 
 function addNumbers() {
-    sums.push(parseInt(numberString));
-    operations.push("+");
+    sum.push(parseInt(numberString));
+    sum.push("+");
     clearNumber();
 }
 
 function minusNumbers() {
-    sums.push(parseInt(numberString));
-    operations.push("-");
+    sum.push(parseInt(numberString));
+    sum.push("-");
     clearNumber();
 }
 
 function divideNumbers() {
-    sums.push(parseInt(numberString));
-    operations.push("/");
+    sum.push(parseInt(numberString));
+    sum.push("/");
     clearNumber();
 }
 
 function multiplyNumbers() {
-    sums.push(parseInt(numberString));
-    operations.push("*");
+    sum.push(parseInt(numberString));
+    sum.push("*");
     clearNumber();
 }
 
 function equalNumbers() {
-    sums.push(parseInt(numberString));
-    switch (operations[0]) {
-        case "+":
-            answer = sums[0] + sums[1];
-            document.getElementById('displayWindow').innerHTML = answer;
-            break;
-        case "-":
-            answer = sums[0] - sums[1];
-            document.getElementById('displayWindow').innerHTML = answer;
-            break;
-        case "/":
-            answer = sums[0] / sums[1];
-            document.getElementById('displayWindow').innerHTML = answer;
-            break;
-        case "*":
-            answer = sums[0] * sums[1];
-            document.getElementById('displayWindow').innerHTML = answer;
-            break;
-    }
+    sum.push(parseInt(numberString));
+    let sumString = sum.join("");
+    answer = eval(sumString);
+    document.getElementById('displayWindow').innerHTML = answer;
+    sum = [];
+    sum.push(answer);
 }
